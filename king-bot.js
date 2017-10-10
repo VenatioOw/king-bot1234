aea5b1c46fddc381394813c3078da71f88aa6180
VenatioOw <32674075+VenatioOw@users.noreply.github.com>
2017-10-10 15:14:59 +0200

    Update king-bot.js

--- king-bot.js
+++ king-bot.js
@@ -17,21 +17,8 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message ,evt) {
-	if (message.substring(0, 1) == '!') {
-        	var args = message.substring(1).split(' ');
-        	var cmd = args[0];
-       
-       		args = args.splice(1);
-        	switch(cmd) {
-        		// !ping
-        		case 'ping':
-        			bot.sendMessage({
-        				to: channelID,
-        				message: 'Pong!'
-        			});
-        			break;
-        	// Just add any case commands if you want to..
-        	}
+	if (msg.content == '!ping') {
+        	msg.reply('Pong!')
   	}
 	if(user == 'Shadowdancer') {
 		if(message.substring(0, 1) == '@') {
