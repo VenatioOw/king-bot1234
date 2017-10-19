5b44fc43ba0f93e53de18a2b1a770763261e9679
VenatioOw <32674075+VenatioOw@users.noreply.github.com>
2017-10-19 21:17:33 +0200

    Update king-bot.js

--- king-bot.js
+++ king-bot.js
@@ -68,4 +68,20 @@ bot.on('message', function (user, userID, channelID, message ,evt) {
 			}
 		}
 	}
+	if(user == 'Ryuu') {
+		if(message.substring(0, 1) == '@') {
+			var args = message.substring(1).split(' ');
+			var cmd = args[0];
+			
+			args = args.splice(1);
+			switch(cmd) {
+				case 'everyone':
+					bot.sendMessage({
+						to: channelID,
+							message: 'The Dick has spoken!'
+					});
+				break;
+			}
+		}
+	}
 });
