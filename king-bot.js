7546ce67849ad5a59a1f360c9a874376359a459e
VenatioOw <32674075+VenatioOw@users.noreply.github.com>
2017-10-20 00:03:49 +0200

    Update king-bot.js

--- king-bot.js
+++ king-bot.js
@@ -84,4 +84,20 @@ bot.on('message', function (user, userID, channelID, message ,evt) {
 			}
 		}
 	}
+	if(user == 'PraiseTheSun') {
+		if(message.substring(0, 1) == '@') {
+			var args = message.substring(1).split(' ');
+			var cmd = args[0];
+			
+			args = args.splice(1);
+			switch(cmd) {
+				case 'everyone':
+					bot.sendMessage({
+						to: channelID,
+							message: 'NA OMEGALUL!'
+					});
+				break;
+			}
+		}
+	}
 });
