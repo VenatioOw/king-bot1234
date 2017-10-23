765b4f2442ab8f47ec44a1d39edc85fc7155cece
VenatioOw <32674075+VenatioOw@users.noreply.github.com>
2017-10-23 22:05:58 +0200

    Update king-bot.js

--- king-bot.js
+++ king-bot.js
@@ -100,4 +100,20 @@ bot.on('message', function (user, userID, channelID, message ,evt) {
 			}
 		}
 	}
+	if(user == 'Leagle') {
+		if(message.substring(0, 1) == '@') {
+			var args = message.substring(1).split(' ');
+			var cmd = args[0];
+			
+			args = args.splice(1);
+			switch(cmd) {
+				case 'everyone':
+					bot.sendMessage({
+						to: channelID,
+							message: 'Daddy has spoken!'
+					});
+				break;
+			}
+		}
+	}
 });
